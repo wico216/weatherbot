629dc3f2ef236545279fb4fa0793f8b4b7f13788
Wiko De La Rosa <wico216@gmail.com>
2024-01-22 22:03:45 -0500

    added response for one city

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import requests
+import socket
+from pprint import pp #to print the json in a pretty way.
 import os
 from dotenv import load_dotenv
 
 load_dotenv()# load environment variables from .env.
 
 APIKEY=os.getenv('APIKEY')# get the API key from the environment variable.
+hostname=socket.gethostname()# get the hostname of the computer.
+ip=socket.gethostbyname(hostname)# get the ip address of the computer.
 
-userzipcode = input("Enter your zipcode: ")
 
-url = "http://api.weatherapi.com/v1/current.json?key="+APIKEY+"&q="+userzipcode
+
+cityname = input("Enter a city name: ")
+#userpref = input("Enter a temperature preference (F or C): ")
+
+url = "http://api.weatherapi.com/v1/current.json?q="+cityname+"&key="+APIKEY
+
 response = requests.get(url)
-
 response_json = response.json()
+date, time = response_json['location']['localtime'].split(" ")# split the date and time from the localtime string.
 
 
-print(f"Current temperature in {response_json['location']['name']} is {response_json['current']['temp_f']} degrees Fahrenheit.")
+
+
+try:
+    print(f"Weather in {response_json['location']['name']} is {response_json['current']['condition']['text']}, temperature is {response_json['current']['temp_f'] } F and time is {time}")
+    #pp(response_json)
+except:
+    print("Error: " + response_json['error']['message'])
